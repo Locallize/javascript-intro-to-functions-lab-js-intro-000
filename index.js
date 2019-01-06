52b054760bdf7ad925ea10311d89bd30fc842a09
beneficial-kernel-0493 <fillanlee@aol.com>
2019-01-06 02:15:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function shout (string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
+  
+}
+
+function sayHiToGrandma(string){
+  if (string ==='I love you, Grandma.'){
+    return "I love you, too.";
+  }
+  
 }
